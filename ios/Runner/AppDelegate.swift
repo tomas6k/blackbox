54bab47692cdb321254642cffa619655f66f9980
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-08 16:42:57 +0000

    Updating to latest FlutterFlow output.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,5 @@
 import UIKit
-import OneSignalFramework
+
 import Flutter
 
 @UIApplicationMain
@@ -9,17 +9,6 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-      OneSignal.Debug.setLogLevel(.LL_VERBOSE)
-        
-        // OneSignal initialization
-        OneSignal.initialize("b791e387-1589-4c11-a0b0-680b2967800e", withLaunchOptions: launchOptions)
-        
-        // requestPermission will show the native iOS notification permission prompt.
-        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
-        OneSignal.Notifications.requestPermission({ accepted in
-          print("User accepted notifications: \(accepted)")
-        }, fallbackToSettings: true)
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
