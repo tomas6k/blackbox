529a19a8a28e6d478cca615b9d8f76ebe70ceea5
tomas6k <tomas.cikanauskas@proton.me>
2024-11-08 18:06:46 +0100

    push

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Tomas CIKANAUSKAS on 08/11/2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
